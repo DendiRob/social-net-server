09db98679d6a5909477cd8f9f7fc26eb9b8eab4b
DendiRob <dendiroblek@gmail.com>
2024-02-07 15:17:08 +0400

    fixed main error handler

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -7,6 +7,7 @@ import {
 
 import { ValidationError } from 'yup';
 import { StatusCodes, getReasonPhrase } from 'http-status-codes';
+import { ErrorHandler } from './ErrorHandler.js';
 
 export default function (app: Express) {
   app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -15,7 +16,13 @@ export default function (app: Express) {
         message: getReasonPhrase(StatusCodes.BAD_REQUEST),
         payload: err.errors
       });
+    } else if (err instanceof ErrorHandler) {
+      return res
+        .status(err.status)
+        .json({ message: getReasonPhrase(err.status), payload: err.payload });
     }
+
+    console.log(err); // TODO: сделать логгер
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .end(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR));
